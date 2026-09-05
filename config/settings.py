e1912bf5aba0eec4d9487ab80431be7cef8ae613
mohammedfaizalpasha <mohameedfaizal11w@gmail.com>
2026-09-05 09:39:42 -0700

    Allow Render domain

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-bl$k!z#*bz@(k4ceonzagc!e&5a$a2=&!ylqg73grxachypgd2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "turf-booking-cspb.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
