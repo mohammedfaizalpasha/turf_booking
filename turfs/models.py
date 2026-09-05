95ec5d2947705c7ef46fc8d96c6a8c612df06c8c
mohammedfaizalpasha <mohameedfaizal11w@gmail.com>
2026-09-05 14:59:09 -0700

    Add turf slots

--- turfs/models.py
+++ turfs/models.py
@@ -20,3 +20,42 @@ class Turf(models.Model):
 
     def __str__(self):
         return self.name
+    
+class TurfSlot(models.Model):
+    turf = models.ForeignKey(
+        Turf,
+        on_delete=models.CASCADE,
+        related_name="slots"
+    )
+
+    date = models.DateField()
+
+    start_time = models.TimeField()
+
+    end_time = models.TimeField()
+
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=[
+                    "turf",
+                    "date",
+                    "start_time"
+                ],
+                name="unique_turf_slot"
+            )
+        ]
+        ordering = ["date", "start_time"]
+
+    def __str__(self):
+        return (
+            f"{self.turf.name} | "
+            f"{self.date} | "
+            f"{self.start_time} - {self.end_time}"
+        )    
